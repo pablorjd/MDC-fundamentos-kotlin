ab07b42a912df7191138783262064772ea5a9231
pablorjd1 <pablo.jimenez2221@gmail.com>
2023-09-22 11:55:32 -0300

    implement material components

--- app/src/main/java/com/cursosant/mdc/ScrollingActivity.kt
+++ app/src/main/java/com/cursosant/mdc/ScrollingActivity.kt
@@ -1,10 +1,12 @@
 package com.cursosant.mdc
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.webkit.URLUtil
 import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -40,10 +42,10 @@ class ScrollingActivity : AppCompatActivity() {
         }
         binding.scrollingContent.btnComprar.setOnClickListener {
             Snackbar.make(it, getString(R.string.comprar), Snackbar.LENGTH_LONG)
-                .setAction(getString(R.string.card_to_go), {
-                    Toast.makeText(this,getString(R.string.go_to),Toast.LENGTH_LONG)
-                         .show()
-                })
+                .setAction(getString(/* resId = */ R.string.card_to_go)) {
+                    Toast.makeText(this, getString(R.string.go_to), Toast.LENGTH_LONG)
+                        .show()
+                }
                 .setAnchorView(binding.fab)
                 .show()
         }
@@ -52,26 +54,74 @@ class ScrollingActivity : AppCompatActivity() {
             binding.scrollingContent.tilPassword.isEnabled = !binding.scrollingContent.tilPassword.isEnabled
         }
 
-        binding.scrollingContent.etUrl.onFocusChangeListener = View.OnFocusChangeListener { view, focused ->
-            val url = binding.scrollingContent.etUrl.text.toString()
-            if ( !focused ) {
-                loadImage(url)
-                // Toast.makeText(this,"Pierde el foco", Toast.LENGTH_LONG).show()
-            }
-        }
-
         Glide.with(this)
             .load("https://www.adslzone.net/app/uploads-adslzone.net/2020/04/android.jpg")
             .diskCacheStrategy(DiskCacheStrategy.ALL)
             .centerCrop()
             .into(binding.scrollingContent.imgCover)
+       // loadImage()
+        binding.scrollingContent.etUrl.onFocusChangeListener = View.OnFocusChangeListener { _, focused ->
+            val url = binding.scrollingContent.etUrl.text.toString()
+            var errorStr:String? = null
+
+            if ( !focused ) {
+                if ( url.isEmpty() ) {
+                    errorStr = getString(R.string.card_required)
+                } else if (URLUtil.isValidUrl(url)) {
+                    loadImage(url)
+                } else {
+
+                    errorStr = getString(R.string.card_url_invalid_url)
+                }
+            }
+            binding.scrollingContent.tilUrl.error = errorStr
+        }
+
+        binding.scrollingContent.toggleButton.addOnButtonCheckedListener { _, checkedId, isChecked ->
+            if (isChecked) {
+                binding.scrollingContent.root.setBackgroundColor (
+                    when (checkedId) {
+                        R.id.btnRed -> Color.RED
+                        R.id.btnBlue -> Color.BLUE
+                        else -> Color.GREEN
+                    }
+                )
+            }
+        }
+       // loadImage()
+
+        binding.scrollingContent.swFab.setOnCheckedChangeListener { button, isCheck ->
+            if ( isCheck ) {
+                button.text = getString(R.string.card_hide_fab)
+                binding.fab.show()
+            } else {
+                button.text = getString(R.string.show_fab)
+                binding.fab.hide()
+            }
+        }
+
+        binding.scrollingContent.sldVol.addOnChangeListener { slider, value, fromUser ->
+            binding.scrollingContent.tvSubTitle.text = "Vol: ${value}"
+        }
+
+        binding.scrollingContent.cpEmail.setOnCheckedChangeListener { chip, isCheck ->
+            if ( isCheck ) {
+                Toast.makeText(this , "${chip.text}", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        binding.scrollingContent.cpEmail.setOnCloseIconClickListener {
+            binding.scrollingContent.cpEmail.visibility = View.GONE
+        }
+
     }
 
-    private fun loadImage(url:String) {
+    private fun loadImage(url:String = "https://www.adslzone.net/app/uploads-adslzone.net/2020/04/android.jpg") {
         Glide.with(this)
             .load(url)
             .diskCacheStrategy(DiskCacheStrategy.ALL)
             .centerCrop()
+            .dontAnimate()
             .into(binding.scrollingContent.imgCover)
     }
 
